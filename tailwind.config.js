98968a1b21bd608b34ec9db51e6d0d6d2dece1a4
Ryan <ryanlaguna28@gmail.com>
2024-03-26 13:58:31 +0800

    Tailwind Reinstallation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./public/**/*.{html,js}",
+    "./src/**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
